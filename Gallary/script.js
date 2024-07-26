c8ff8e97a0a014bb067783e78b918d2602057d5c
OCTEXA <140159523+OCTEXA@users.noreply.github.com>
2024-07-26 19:40:52 +0530

    Delete Gallary directory

--- Gallary/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('mousemove', (e) => {
-    const cursorGlow = document.querySelector('.cursor-glow');
-    cursorGlow.style.left = e.pageX + 'px';
-    cursorGlow.style.top = e.pageY + 'px';
-});
